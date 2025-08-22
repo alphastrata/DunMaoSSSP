35b105c5602ca34a42c539553cac3f6d24bed9dd
jer <alphastrata@gmail.com>
2025-08-22 14:16:37 +1000

    wip: Graph::to_petgraph -> petgraph::Graph and impl

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ pub mod utils;
 #[cfg(feature = "parallel")]
 pub mod parallel;
 
+#[cfg(feature = "petgraph")]
+pub mod petgraph_utils;
+
 pub use graph::{Edge, Graph};
 pub use sequential::SSSpSolver;
 pub use sequential_v2::DuanMaoSolverV2;
